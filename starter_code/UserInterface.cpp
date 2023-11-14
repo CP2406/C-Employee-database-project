98125a1f37bf4ac140c187e4e6694f42930a71b1
Amy_yanqing <yanqing.zhong@my.jcu.edu.au.com>
2023-11-14 13:48:17 +1000

    Add Start-Time Debug Mode

--- starter_code/UserInterface.cpp
+++ starter_code/UserInterface.cpp
@@ -2,6 +2,10 @@
 #include <stdexcept>
 #include <exception>
 #include "Database.h"
+#include <cstddef>
+#include <fstream>
+
+
 
 using namespace std;
 using namespace Records;
@@ -11,6 +15,34 @@ void doHire(Database& db);
 void doFire(Database& db);
 void doPromote(Database& db);
 void doDemote(Database& db);
+
+#define DEBUG_MODE
+#ifdef DEBUG_MODE
+    class Logger
+    {
+    public:
+        template<typename... Args>
+        static void log(const Args&... args)
+        {
+            ofstream logfile { msDebugFileName, ios_base::app };
+            if (logfile.fail()) {
+            cerr << "Unable to open debug file!" << endl;
+            return;
+            }
+        // Use a C++17 unary right fold, see Chapter 26.
+        ((logfile << args),...);
+        logfile << endl;
+        }
+
+    private:
+    static const string msDebugFileName;
+    };
+    const string Logger::msDebugFileName="debugfile.out";
+
+    #define log(...) Logger::log(__func__, "(): ", __VA_ARGS__)
+#else
+    #define log(...)
+#endif
 
 int main()
 {
