a700db93361ab5ec751562be832aba8d1ad53da8
Amy_yanqing <yanqing.zhong@my.jcu.edu.au.com>
2023-11-26 22:39:00 +1000

    Add user database to case 12 option

--- starter_code/UserInterface.cpp
+++ starter_code/UserInterface.cpp
@@ -19,215 +19,224 @@ void doDemote(Database& db);
 void doEdit(Database& db);
 
 Database makeNewDatabase();
+// Database newUserDatabase();
 
 int main()
-{
-    log("started");
-	Database employeeDB;
+    {
+        log("started");
+        Database employeeDB;
+        Database userDB;
 
-	bool done = false;
-	while (!done) {
-		int selection = displayMenu();
-		switch (selection) {
-		case 0:
-            log("case 0");
-			done = true;
-			break;
-		case 1:
-			doHire(employeeDB);
-            log("case 1");
-			break;
-		case 2:
-			doFire(employeeDB);
-			break;
-		case 3:
-			doPromote(employeeDB);
-			break;
-		case 4:
-			employeeDB.displayAll();
-			break;
-		case 5:
-			employeeDB.displayCurrent();
-			break;
-		case 6:
-			employeeDB.displayFormer();
-			break;
-        case 7:
-            employeeDB=makeNewDatabase();
-            break;
-        case 8:
-            employeeDB.saveToFile();
-            break;
-        case 9:
-            employeeDB.loadFromFile();
-            break;
-        case 10:
-            doEdit(employeeDB);
-            break;
-        case 11:
-           employeeDB.searchMenu();
-            break;
-        case 12:
-            employeeDB.loginMenu();
+        bool done = false;
+        while (!done) {
+            int selection = displayMenu();
+            switch (selection) {
+            case 0:
+                log("case 0");
+                done = true;
+                break;
+            case 1:
+                doHire(employeeDB);
+                log("case 1");
+                break;
+            case 2:
+                doFire(employeeDB);
+                break;
+            case 3:
+                doPromote(employeeDB);
+                break;
+            case 4:
+                employeeDB.displayAll();
+                break;
+            case 5:
+                employeeDB.displayCurrent();
+                break;
+            case 6:
+                employeeDB.displayFormer();
+                break;
+            case 7:
+                employeeDB=makeNewDatabase();
+                break;
+            case 8:
+                employeeDB.saveToFile();
+                break;
+            case 9:
+                employeeDB.loadFromFile();
+                break;
+            case 10:
+                doEdit(employeeDB);
+                break;
+            case 11:
+            employeeDB.searchMenu();
+                break;
+            case 12:
+                 userDB=userDB.newUserDatabase();
+                 userDB.loginMenu();
+                break;
+            default:
+                cerr << "Unknown command." << endl;
+                log("case default");
+                break;
+            }
+        }
 
-        default:
-			cerr << "Unknown command." << endl;
-            log("case default");
-			break;
-		}
-	}
-
-	return 0;
-   
-}
+        return 0;
+    
+    }
 
 int displayMenu()
-{
-	// Note:
-	//		One important note is that this code assumes that the user will
-	//		"play nice" and type a number when a number is requested.
-	//		When you read about I/O in Chapter 13, you will learn how to
-	//		protect against bad input.
+    {
+        // Note:
+        //		One important note is that this code assumes that the user will
+        //		"play nice" and type a number when a number is requested.
+        //		When you read about I/O in Chapter 13, you will learn how to
+        //		protect against bad input.
 
-    int selection;
+        int selection;
 
-    cout << endl;
-    cout << "Employee Database" << endl;
-    cout << "-----------------" << endl;
-    cout << "1) Hire a new employee" << endl;
-    cout << "2) Fire an employee" << endl;
-    cout << "3) Promote an employee" << endl;
-    cout << "4) List all employees" << endl;
-    cout << "5) List all current employees" << endl;
-    cout << "6) List all former employees" << endl;
-    cout << "7) Make new database" << endl;
-    cout << "8) Save database to file" << endl;
-    cout << "9) Load database from file" << endl;
-    cout << "10) Edit employee" << endl;
-    cout << "11) Search employee" << endl;
-    cout << "12) login menu" << endl;
-    cout << "0) Quit" << endl;
-    cout << endl;
-    cout << "---> ";
-    
-	cin >> selection;
-    
-	return selection;
-}
+        cout << endl;
+        cout << "Employee Database" << endl;
+        cout << "-----------------" << endl;
+        cout << "1) Hire a new employee" << endl;
+        cout << "2) Fire an employee" << endl;
+        cout << "3) Promote an employee" << endl;
+        cout << "4) List all employees" << endl;
+        cout << "5) List all current employees" << endl;
+        cout << "6) List all former employees" << endl;
+        cout << "7) Make new database" << endl;
+        cout << "8) Save database to file" << endl;
+        cout << "9) Load database from file" << endl;
+        cout << "10) Edit employee" << endl;
+        cout << "11) Search employee" << endl;
+        cout << "12) login menu" << endl;
+        cout << "0) Quit" << endl;
+        cout << endl;
+        cout << "---> ";
+        
+        cin >> selection;
+        
+        return selection;
+    }
 
 void doHire(Database& db)
-{   
-    log("start");
-    string firstName;
-    string lastName;
+    {   
+        log("start");
+        string firstName;
+        string lastName;
 
-    cout << "First name? ";
-    cin >> firstName;
-    cout << "Last name? ";
-    cin >> lastName;
-    
-    db.addEmployee(firstName, lastName);
-    log("end");
-}
+        cout << "First name? ";
+        cin >> firstName;
+        cout << "Last name? ";
+        cin >> lastName;
+        
+        db.addEmployee(firstName, lastName);
+        log("end");
+    }
 
 void doFire(Database& db)
-{  
-    int employeeNumber;
+    {  
+        int employeeNumber;
 
-    cout << "Employee number? ";
-    cin >> employeeNumber;
- 
+        cout << "Employee number? ";
+        cin >> employeeNumber;
+    
 
-    try {
-        Employee& emp = db.getEmployee(employeeNumber);
-        emp.fire();
-        cout << "Employee " << employeeNumber << " terminated." << endl;
-    } catch (const std::logic_error& exception) {
-        cerr << "Unable to terminate employee: " << exception.what() << endl;
+        try {
+            Employee& emp = db.getEmployee(employeeNumber);
+            emp.fire();
+            cout << "Employee " << employeeNumber << " terminated." << endl;
+        } catch (const std::logic_error& exception) {
+            cerr << "Unable to terminate employee: " << exception.what() << endl;
+        }
     }
-}
 
 void doPromote(Database& db)
-{
-    int employeeNumber;
-    int raiseAmount;
+    {
+        int employeeNumber;
+        int raiseAmount;
 
-    cout << "Employee number? ";
-    cin >> employeeNumber;
-    cout << "How much of a raise? ";
-    cin >> raiseAmount;
+        cout << "Employee number? ";
+        cin >> employeeNumber;
+        cout << "How much of a raise? ";
+        cin >> raiseAmount;
 
-    try {
-        Employee& emp = db.getEmployee(employeeNumber);
-        emp.promote(raiseAmount);
-        emp.display();
-    } catch (const std::logic_error& exception) {
-        cerr << "Unable to promote employee: " << exception.what() << endl;
+        try {
+            Employee& emp = db.getEmployee(employeeNumber);
+            emp.promote(raiseAmount);
+            emp.display();
+        } catch (const std::logic_error& exception) {
+            cerr << "Unable to promote employee: " << exception.what() << endl;
+        }
     }
-}
 
 
 void doEdit(Database& db)
-{
-     int employeeNumber;
-    cout << "Employee number? ";
-    cin >> employeeNumber;
+    {
+        int employeeNumber;
+        cout << "Employee number? ";
+        cin >> employeeNumber;
 
 
-    try {
-        Employee& emp = db.getEmployee(employeeNumber);
-        bool done = false;
+        try 
+        {
+            Employee& emp = db.getEmployee(employeeNumber);
+            bool done = false;
 
-        while(!done){
-            emp.display();
-            int intOption;
-			cout << "1) Edit by salary" << endl;
-			cout << "2) Edit by address" << endl;
-			cout << "3) Edit hired" << endl;
-			cout << "0) Quit" << endl;
-            cin>>intOption;
-            int newSalary;
-            string newAddress; 
-            bool hireStatus;            
-            switch(intOption){
-                case 0:
-                    cout<<"Quit editing"<<endl;
-                    done=true;
-                    break;
-                case 1:   
-                    cout<<"Enter New Salary: "<<endl;
-                    cin>>newSalary;         
-                    emp.setSalary(newSalary);
-                    break; 
+            while(!done)
+            {
+                emp.display();
+                int intOption;
+                cout << "1) Edit by salary" << endl;
+                cout << "2) Edit by address" << endl;
+                cout << "3) Edit hired" << endl;
+                cout << "0) Quit" << endl;
+                cin>>intOption;
+                int newSalary;
+                string newAddress; 
+                bool hireStatus;            
+                switch(intOption)
+                {
+                    case 0:
+                        cout<<"Quit editing"<<endl;
+                        done=true;
+                        break;
+                    case 1:   
+                        cout<<"Enter New Salary: "<<endl;
+                        cin>>newSalary;         
+                        emp.setSalary(newSalary);
+                        break; 
 
-                 case 2:
-                    cout<<"Enter New Address: "<<endl; 
-                    cin.ignore();
-                    getline(cin,newAddress);                  
-                    emp.setAddress(newAddress);
-                    break;  
-                case 3: 
-                    cout << "Edit to: Enter '1' to hire or '0' to fire: ";
-                    cin >> hireStatus; 
-                    if (hireStatus) {
-                        emp.hire();
-                         cout<<"Hire Status Updated to : Hired" << endl;
-                    } else {
-                        emp.fire();
-                        cout << "Hire Status Updated to : Fired" << endl;
-                    }  
-                    break;
-                    
+                    case 2:
+                        cout<<"Enter New Address: "<<endl; 
+                        cin.ignore();
+                        getline(cin,newAddress);                  
+                        emp.setAddress(newAddress);
+                        break;  
+                    case 3: 
+                        cout << "Edit to: Enter '1' to hire or '0' to fire: ";
+                        cin >> hireStatus; 
+                        if (hireStatus) {
+                            emp.hire();
+                            cout<<"Hire Status Updated to : Hired" << endl;
+                        } else {
+                            emp.fire();
+                            cout << "Hire Status Updated to : Fired" << endl;
+                        }  
+                        break;
+                        
                     emp.display();
 
-                    }
+                }
 
             }
 
-    } catch (const std::logic_error& exception) {
-        cerr << "Unable to promote employee: " << exception.what() << endl;
+        } 
+        catch (const std::logic_error& exception) 
+            {
+                cerr << "Unable to promote employee: " << exception.what() << endl;
+            }
+
     }
-}
 
 
 Database makeNewDatabase()
@@ -276,6 +285,6 @@ Database makeNewDatabase()
             }
         }
         return db;
-        
-
     }
+
+
