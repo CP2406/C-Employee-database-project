181eb72445f4f0b8efc49cee05ee90e9ee4b0fea
Amy_yanqing <yanqing.zhong@my.jcu.edu.au.com>
2023-11-26 18:48:01 +1000

    Add default id& password default values

--- starter_code/Database.h
+++ starter_code/Database.h
@@ -7,19 +7,29 @@
 
 namespace Records {
 	const int kFirstEmployeeNumber = 1000;
+	const int kFirstUerId =0;
+	const std::string KFirstPassword{"admin"};
 
 	class Database
 	{
 	public:
 		Employee& addEmployee(Employee& emp1);	
+			
+
 		Employee& addEmployee(const std::string& firstName,
-							  const std::string& lastName);						  		  
+							  const std::string& lastName);	
+
+		Employee& createUser(const int& id, const std::string& password);
+
 		Employee& addEmployee(
 			const std::string& firstName,
 			const std::string& middleName,
 			const std::string& lastName);
 
 		Employee& getEmployee(int employeeNumber);
+
+		Employee& getUser(int UserIdNumber);
+		
 		Employee& getEmployee(const std::string& firstName,
 							  const std::string& lastName);
 
@@ -30,14 +40,14 @@ namespace Records {
 		void loadFromFile();
 		void searchMenu() const;
 		void loginMenu();
-		// Database searchByFirstName() const;
-	
-	
 	
 
 	private:
 		std::vector<Employee> mEmployees;
+		std::vector<Employee> mUsers;
 		int mNextEmployeeNumber = kFirstEmployeeNumber;
+		int mNextUserId = kFirstUerId;
+		std::string mNextUserPassword=KFirstPassword;
 		enum class SearchOption{
 			BY_FIRST,BY_MIDDLE,BY_LAST,BY_ADDRESS
 
